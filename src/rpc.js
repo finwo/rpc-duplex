08c5356ce692c2efcffc6edf9b9583ac0ffcd5bc
Robin Bron <finwo@pm.me>
2019-06-14 11:40:57 +0200

    Fixed some memory leaks; removed notepack.io dep

--- src/rpc.js
+++ src/rpc.js
@@ -2,14 +2,34 @@ const through            = require('through'),
       packetize          = require('stream-packetize'),
       serializeError     = require('serialize-error'),
       nagle              = require('stream-nagle'),
-      { encode, decode } = require('notepack.io'),
-      stream             = Symbol();
+      stream             = Symbol('stream'),
+      isBuffer           = require('is-buffer');
+
+function encode( subject ) {
+  return JSON.stringify(subject);
+}
+
+function decode( subject ) {
+  try {
+    return JSON.parse(subject, (k,v) => {
+      if (  'object' !== typeof v      ) return v;
+      if (                     !v      ) return v;
+      if (!('type'   in         v     )) return v;
+      if (  'Buffer' !==        v.type ) return v;
+      if (!('data'   in         v     )) return v;
+      if (! Array.isArray(v.data      )) return v;
+      return Buffer.from(v.data);
+    });
+  } catch(e) {
+    console.error(e);
+  }
+}
 
 // Generate a safe ID to use
 function genId() {
   let out = '';
   while (out.length < 48)
-    out += (Math.random() * 36 * 36).toString(36).substr(0, 2);
+    out += Math.floor(Math.random() * 36).toString(36);
   return out;
 }
 
@@ -54,6 +74,7 @@ function attachStream( obj, s ) {
 function serialize( data, out, path, includeFn ) {
   out      = out  || [];
   path     = path || [];
+
   let type = 1;
   Object.keys(data).forEach(function(key) {
     let current = [path.concat([key]), typeof data[key]];
@@ -100,7 +121,7 @@ function deserialize( ref, data, obj ) {
     let ref                 = obj;
     let lastKey             = path.pop();
 
-    // Get the proper reference
+    // Follow path into object
     for (const key of path) {
       ref = ref[key] = ref[key] || {};
     }
@@ -109,47 +130,43 @@ function deserialize( ref, data, obj ) {
     switch(type) {
       case 'null':
         ref[lastKey] = null;
-        break;
+        return;
       case 'object':
-        if ('object' !== typeof ref[lastKey]) {
+        if ('object' !== typeof ref[lastKey])
           ref[lastKey] = {};
-        }
         return;
       case 'array':
-        if (!Array.isArray(ref[lastKey])) {
+        if (!Array.isArray(ref[lastKey]))
           ref[lastKey] = [];
-        }
         return;
+      case 'callback':
+        return ref[lastKey] = function(...args) {
+          s.output.write(encode({
+            ret : value,
+            arg : serializeArgs(s,args),
+          }));
+        };
       case 'function':
         if (!value) value = path.concat([lastKey]);
         if (!Array.isArray(value)) value = [value];
-        ref[lastKey] = function (...args) {
+        return ref[lastKey] = function (...args) {
           return new Promise((resolve, reject) => {
-            let resolveId      = genId(),
-                rejectId       = genId();
-            s.local[resolveId] = function (data) {
-              delete s.local[resolveId];
-              delete s.local[rejectId];
+            let callId = genId();
+            s.tmp[callId] = function( err, data ) {
+              delete s.tmp[callId];
+              if (err) return reject(new RemoteError(err));
               resolve(data);
-            };
-            s.local[rejectId]  = function (data) {
-              delete s.local[resolveId];
-              delete s.local[rejectId];
-              reject(new RemoteError(data));
             };
             s.output.write(encode({
               fn : value,
+              id : callId,
               arg: serializeArgs(s, args),
-              ret: [resolveId],
-              err: [rejectId]
             }));
           });
         };
-        break;
       default:
-        if (value !== ref[lastKey]) {
+        if (value !== ref[lastKey])
           ref[lastKey] = value;
-        }
         return;
     }
   });
@@ -162,9 +179,14 @@ function serializeArgs(ref, data) {
   serialized.forEach(function(token) {
     switch(token[1]) {
       case 'function':
+        let fn = token[2];
         let id = genId();
-        s.local[id] = token[2];
-        token[2]    = id;
+        s.tmp[id] = function(...args) {
+          delete s.tmp[id]
+          fn(...args);
+        };
+        token[1]  = 'callback';
+        token[2]  = id;
         break;
     }
   });
@@ -189,12 +211,11 @@ const rpc = module.exports = function (options, local, remote) {
   // Ensure local & remote objects
   local      = local  || {};
   remote     = remote || {};
-  let timer  = false;
 
-  // Create the loop
-  let input  = packetize.decode(opts);
-  let output = packetize.encode(opts);
-  let io = through(function (data) {
+  // Create IO loop
+  const input  = packetize.decode(opts);
+  const output = packetize.encode(opts);
+  const io     = through(function (data) {
     input.write(data);
   }, function() {
     input.write(null);
@@ -205,84 +226,53 @@ const rpc = module.exports = function (options, local, remote) {
     })
     .on('end', function() {
       io.queue(null);
+      io.emit('close');
     });
+
+  // Internal functions
+  const internal = {
+    state: function() {
+      return io.output.write(encode({fn:'update',arg:[serialize(io.local)]}));
+    },
+    update: function( serialized ) {
+      deserialize(io,serialized,io.remote);
+      return io.emit('update');
+    },
+  };
 
   // Handle incoming data
   input.on('data', async function(data) {
     if (!data) return;
     data = decode(data);
 
-    // Send ping after idle
-    // Stop the keepalive (temporary solution)
-    if ('stopKeepalive' === data.fn) {
-      if (!timer) return;
-      clearTimeout(timer);
-      timer = false;
-      io.output.write(encode({ fn : 'stopKeepalive' }));
-      return;
+    // Handle internal functions
+    if (('string' === typeof data.fn) && (data.fn in internal)) {
+      return internal[data.fn](...(data.arg||[]));
     }
 
-    if (timer) clearTimeout(timer);
-    if (opts.keepalive) {
-      timer = setTimeout(function() {
-        timer = false;
-        io.output.write(encode({ fn: 'ping' }));
-      }, opts.keepalive);
+    // Handle function returns
+    if (('string' === typeof data.ret) && (data.ret in io.tmp)) {
+      return io.tmp[data.ret](...deserializeArgs(io,data.arg));
     }
 
-    // Respond to state request
-    if ('state' === data.fn) {
-      io.output.write(encode({
-        fn : 'update',
-        arg: serialize(io.local)
-      }));
-      return;
-    }
-
-    // Pong = fine
-    if ('pong' === data.fn) {
-      return;
-    }
-
-    // Reply to pings
-    if ('ping' === data.fn) {
-      io.output.write(encode({ fn: 'pong' }));
-      return;
-    }
-
-    // Handle incoming update
-    if ('update' === data.fn) {
-      deserialize(io, data.arg, io.remote);
-      io.emit('update');
-      return;
-    }
-
-    // Handle a function call
+    // Handle local functions
     if (Array.isArray(data.fn)) {
       let target = io.local;
-      for ( let key of data.fn ) {
+      for( let key of data.fn )
         target = target[key] || {};
-      }
-      if ('function' !== typeof target) {
-        return;
-      }
+      if ('function' !== typeof target) return;
       try {
-        let result = await target(...deserializeArgs(io, data.arg));
-        if (data.ret) {
-          io.output.write(encode({
-            fn : data.ret,
-            arg: serializeArgs(io, [result]),
-          }));
-        }
+        let result = await target(...deserializeArgs(io,data.arg));
+        return io.output.write(encode({
+          ret : data.id,
+          arg : serializeArgs(io,[null,result]),
+        }));
       } catch(e) {
-        if (data.err) {
-          io.output.write(encode({
-            fn : data.err,
-            arg: serializeArgs(io, [serializeError(e)])
-          }));
-        }
+        return io.output.write(encode({
+          ret : data.id,
+          arg : serializeArgs(io,[serializeError(e)]),
+        }));
       }
-      return;
     }
   });
 
@@ -291,10 +281,11 @@ const rpc = module.exports = function (options, local, remote) {
   io.remote  = remote;
   io.output  = output;
   io.input   = input;
+  io.tmp     = {};
   io[stream] = io;
   attachStream(io.remote,io);
   attachStream(io.local ,io);
-
+ 
   // Return the duplex
   return io;
 };
@@ -310,16 +301,11 @@ rpc.local = function(ref) {
 
 rpc.update = function(ref) {
   if (!ref[stream]) return;
-  ref[stream].output.write(encode({fn: 'state'}));
+  ref[stream].output.write(encode({fn:'state'}));
 };
 
 rpc.updateRemote = function(ref) {
   if (!ref[stream]) return;
   attachStream(ref,ref[stream]);
   ref[stream].input.emit('data',encode({fn: 'state'}));
-};
-
-rpc.stopKeepalive = function(ref) {
-  if (!ref[stream]) return;
-  ref[stream].input.emit('data', encode({fn: 'stopKeepalive'}));
 };
